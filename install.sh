04819ca45cd977690fdf3e8a06c86727f8643469
Dmitry Kolmakov <ds.kolmakov@gmail.com>
2019-09-06 18:47:51 +0300

    Cut leading command sequence number from string conveyed to database

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 
-export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then /home/dmitry/projects/hm/build/hm-insert /home/dmitry/projects/hm/build/history.db "$(pwd)" "$(history 1)"; fi'
+export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then /home/dmitry/projects/hm/build/hm-insert /home/dmitry/projects/hm/build/history.db "$(pwd)" "$(history 1 | sed -E "s/^ *[0-9]* *//")"; fi'
 
