2e2c5c14b07547766a097c97a0992a0019e44e25
Dmitry Kolmakov <ds.kolmakov@gmail.com>
2019-09-17 17:46:59 +0300

    Fixed test database removal procedure to work from the root of project

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,9 @@ import pytest
 
 @pytest.fixture
 def remove_db():
-    for _, _, files in os.walk('.'):
+    for dirpath, _, files in os.walk('.'):
         for f in files:
             _, ext = os.path.splitext(f)
             if ext == ".testdb": 
-                os.remove(f) 
+                os.remove(os.path.join(dirpath, f)) 
 
