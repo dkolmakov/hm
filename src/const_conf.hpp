43f4abbc8d598a8052b55ba17a2c79f5b5d9f1f9
Dmitry Kolmakov <ds.kolmakov@gmail.com>
2019-11-10 22:14:36 +0300

    Fix for #35: scripts in bashrc are executed only if hm-db is in PATH

--- src/const_conf.hpp
+++ src/const_conf.hpp
@@ -19,34 +19,34 @@
 #include <string>
 
 const std::string const_configuration = "\
-# Create history manager home directory\n\
-mkdir -p $hm_home\n\
+\t# Create history manager home directory\n\
+\tmkdir -p $hm_home\n\
 \n\
-# Path to the HM database\n\
-hm_history_db=$hm_home/history.db\n\
+\t# Path to the HM database\n\
+\thm_history_db=$hm_home/history.db\n\
 \n\
-# Path to the temporary history file\n\
-hm_history_tmp=$hm_home/history.tmp\n\
+\t# Path to the temporary history file\n\
+\thm_history_tmp=$hm_home/history.tmp\n\
 \n\
-# Create a new session entry in the database corresponding to the current terminal session\n\
-hm_session_id=$(hm-db $hm_history_db session)\n\
+\t# Create a new session entry in the database corresponding to the current terminal session\n\
+\thm_session_id=$(hm-db $hm_history_db session)\n\
 \n\
-# Service string to split values of stored commands\n\
-separator=\"!!hmseparator!!\"\n\
+\t# Service string to split values of stored commands\n\
+\tseparator=\"!!hmseparator!!\"\n\
 \n\
-# Set a script to be executed after each other command in the current terminal session\n\
-export PROMPT_COMMAND='rc=$?; if [ $(id -u) -ne 0 ] && ! [ -z $hm_session_id ]; then echo ${hm_session_id}${separator}$(date +\"%Y-%m-%d %H:%M:%S\")${separator}$(pwd)${separator}\"$(history 1 | sed -E \"s/^ *[0-9]* *//\")\"${separator}$rc >> $hm_history_tmp; fi'\n\
+\t# Set a script to be executed after each other command in the current terminal session\n\
+\texport PROMPT_COMMAND='rc=$?; if [ $(id -u) -ne 0 ] && ! [ -z $hm_session_id ]; then echo ${hm_session_id}${separator}$(date +\"%Y-%m-%d %H:%M:%S\")${separator}$(pwd)${separator}\"$(history 1 | sed -E \"s/^ *[0-9]* *//\")\"${separator}$rc >> $hm_history_tmp; fi'\n\
 \n\
-# Wrapper for hm-db which adds the selected commands at the beginning of the current terminal history\n\
-hm() {\n\
-SAVE_HISTFILE=$HISTFILE\n\
-HISTFILE=$(mktemp extracted_history.XXXXXXXXXX)\n\
-# Parse commands saved to the text file\n\
-hm-db $hm_history_db parse $hm_history_tmp $separator\n\
-# Update history\n\
-hm-db $hm_history_db select $@ > $HISTFILE && echo hm \"$*\" >> $HISTFILE && history -r\n\
-if [ $? -eq 0 ]; then echo \"History successfully updated!\"; fi\n\
-rm -rf $hm_history_tmp $HISTFILE\n\
-HISTFILE=$SAVE_HISTFILE\n\
-}\n\
+\t# Wrapper for hm-db which adds the selected commands at the beginning of the current terminal history\n\
+\thm() {\n\
+\t\tSAVE_HISTFILE=$HISTFILE\n\
+\t\tHISTFILE=$(mktemp extracted_history.XXXXXXXXXX)\n\
+\t\t# Parse commands saved to the text file\n\
+\t\thm-db $hm_history_db parse $hm_history_tmp $separator\n\
+\t\t# Update history\n\
+\t\thm-db $hm_history_db select $@ > $HISTFILE && echo hm \"$*\" >> $HISTFILE && history -r\n\
+\t\tif [ $? -eq 0 ]; then echo \"History successfully updated!\"; fi\n\
+\t\trm -rf $hm_history_tmp $HISTFILE\n\
+\t\tHISTFILE=$SAVE_HISTFILE\n\
+\t}\n\
 ";
