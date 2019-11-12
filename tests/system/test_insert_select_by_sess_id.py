a0b6a7d536b872986a97ce5d42cd9426a5404645
Dmitry Kolmakov <ds.kolmakov@gmail.com>
2019-11-12 18:12:41 +0300

    Added more tests for by-session selection case

--- tests/system/test_insert_select_by_sess_id.py
+++ tests/system/test_insert_select_by_sess_id.py
@@ -8,4 +8,40 @@ def test_basic_insert_select(remove_db):
 
     insert_select_by_session_name(cmd, sess_name)
 
+def test_insert_select_underscores(remove_db):
+    sess_name = "Some_name_with_underscores"
+    cmd = "some --arbitrary command"
+
+    insert_select_by_session_name(cmd, sess_name)
+
+def test_insert_select_dots(remove_db):
+    sess_name = "Some.name.with.dots"
+    cmd = "some --arbitrary command"
+
+    insert_select_by_session_name(cmd, sess_name)
    
+def test_insert_select_hypens(remove_db):
+    sess_name = "Some-name-with-dots"
+    cmd = "some --arbitrary command"
+
+    insert_select_by_session_name(cmd, sess_name)
+
+def test_insert_select_for_several_sessions(remove_db):
+    first_sess = basic_create_db("test-name")
+    second_sess = basic_create_db("test-name")
+    
+    pwd1 = "/some/arbitrary/path"
+    cmd1 = "some --arbitrary command"
+
+    pwd2 = "/some/arbitrary/path/inside"
+    cmd2 = "some --other command"
+
+    insert(first_sess, pwd1, cmd1, 0)
+    insert(second_sess, pwd2, cmd2, 0)
+    
+    stdout = select_by_session(first_sess)
+
+    assert len(stdout) == 2, "Wrong number of commands!"
+    assert cmd1 == stdout[0], "Wrong command in the database!"
+    assert cmd2 == stdout[1], "Wrong command in the database!"
+
