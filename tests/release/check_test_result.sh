e194e7cfac4d504f003c85aeabc9f58a3965f8c5
Dmitry Kolmakov <ds.kolmakov@gmail.com>
2020-02-19 16:49:52 +0300

    Some other corrections

--- tests/release/check_test_result.sh
+++ tests/release/check_test_result.sh
@@ -3,7 +3,7 @@ rc=0
 
 # Check that the test command is in history (second from the end, since the first is "hm -d /test/path")
 last_command=$(get_last_command)
-if ! [ "$last_command" = "echo test" ]; then echo 'Error!!! Test command not found!'; rc=1; fi
+if ! [ "$last_command" = "echo test" ]; then echo "Error! $last_command != echo test"; rc=1; fi
 
 # Form error code 
 [ $rc -eq 0 ]
