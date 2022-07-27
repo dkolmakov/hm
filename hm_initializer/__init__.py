53f9cbe9f0cc015cd28dac4da5daeb1fab11031d
Dmitry Kolmakov <ds.kolmakov@gmail.com>
2022-07-27 18:10:20 +0300

    Binary hm-db is built and configuration is called

--- hm_initializer/__init__.py
+++ hm_initializer/__init__.py
@@ -1,4 +1,20 @@
-
+import os    
+import sys
+import subprocess
 
 def main():
-    print("hm should be initialized here")
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    hm_db_path = os.path.abspath(os.path.join(cwd, os.pardir))
+    path_modification = "PATH=$PATH:" + hm_db_path + "\n"
+    
+    if len(sys.argv) > 1:
+        with open(sys.argv[1], 'a') as f:
+            f.write(path_modification)
+    else:
+        print(path_modification)
+    
+    cmd = ' '.join([hm_db_path + '/hm-db', 'configure'] + sys.argv[1:])
+    #print(cmd)
+    return_code = subprocess.call(cmd, shell=True)
+
+    #print("hm should be initialized here")
